9bcfa5b50bc3c4c12a7cb8068f4a19094c6135fc
Dhrumil Patel <dhrumilp063@gmail.com>
2023-05-31 20:39:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (81.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,9 +3,7 @@ public:
     bool isValid(string s) {
        stack<char> stc;
        for(auto i : s) {
-           if(i == '(') stc.push('(');
-           else if(i == '{') stc.push('{');
-           else if(i == '[') stc.push('[');
+           if('(' == i || '{' == i || '[' == i) stc.push(i);
            else {
                if(stc.size() == 0) return false;
                else if(stc.top() == '(' && i == ')') stc.pop();
